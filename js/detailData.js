989c163ec44b5f6b771c7c298717130b0ff53664
SergeyEvtush <evtushru208@gmail.com>
2022-09-29 23:11:52 +0200

    final fix

--- js/detailData.js
+++ js/detailData.js
@@ -1,6 +1,7 @@
 //48/51мин
 
 const datailData = () => {
+	const preloader = document.querySelector('.preloder');
 	const renderGanreList = (ganres) => {
 		const dropDownBlock = document.querySelector('.header__menu .dropdown');
 		ganres.forEach((ganre) => {
@@ -9,27 +10,34 @@ const datailData = () => {
 
 	}
 	const renderAnimeDetails = (array, itemId) => {
-		/* 	console.log(array);
-			console.log(itemId); */
+
 		const animeObj = array.find(item => item.id == itemId);
 		const imageBlock = document.querySelector('.anime__details__pic');
 		const viewsBlock = imageBlock.querySelector('.view');
 		const titleBlock = document.querySelector('.anime__details__title h3');
 		const subTitleBlock = document.querySelector('.anime__details__title span');
 		const descriptionBlock = document.querySelector('.anime__details__text p');
-
+		const widgetList = document.querySelectorAll('.anime__details__widget ul li');//breadcrumb__links
+		const breadCrumb = document.querySelector('.breadcrumb__links span');
 		if (animeObj) {
 			console.log(animeObj);
 
 			imageBlock.dataset.setbg = animeObj.image//
-			viewsBlock.innerHTML = '';
 			viewsBlock.insertAdjacentHTML('beforeend', `<i class="fa fa-eye"></i>${animeObj.views}`);
 			titleBlock.textContent = animeObj.title;
 			subTitleBlock.textContent = animeObj['original-title'];
 			descriptionBlock.textContent = animeObj.description;
+			widgetList[0].insertAdjacentHTML('beforeend', `<span>Date aired:</span>${animeObj.date}`);
+			widgetList[1].insertAdjacentHTML('beforeend', `<span>Status:</span>${animeObj.rating}`);
+			widgetList[2].insertAdjacentHTML('beforeend', `<span>Genre:</span> ${animeObj.tags.join(", ")}`);
+			breadCrumb.textContent = animeObj.ganre;
+
 			document.querySelectorAll('.set-bg').forEach((elem) => {
 				elem.style.backgroundImage = `url(${elem.dataset.setbg}) `;
 			});
+			setTimeout(() => {
+				preloader.classList.remove('active');
+			}, 500);
 		}
 		else {
 			console.log('Аниме отсутствует');
@@ -38,7 +46,7 @@ const datailData = () => {
 
 	};
 
-
+	preloader.classList.add('active');
 
 	fetch('./db.json')
 		.then((response) => {
